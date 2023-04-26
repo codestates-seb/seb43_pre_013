5ad52d7c57ea74c406367fa8af6640885acd192b
Reese <ajw5921@gmail.com>
2023-04-26 10:50:36 +0900

    CORS 에러 해결

--- server/src/main/java/com/codestates/stackoverflow/config/WebConfig.java
+++ server/src/main/java/com/codestates/stackoverflow/config/WebConfig.java
@@ -23,8 +23,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*");
-        // .allowedMethods("GET", "POST", "PUT", "DELETE")
-        // .allowedHeaders("*");
+                .allowedOrigins("*")    //허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
